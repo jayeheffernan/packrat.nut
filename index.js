70f4bc69188be8535c3903dbbeb101fb4cf544ce
Jaye Heffernan <jpeheffernan@gmail.com>
2018-11-29 00:11:14 +1100

    Revert to calculator

--- index.js
+++ index.js
@@ -11,73 +11,31 @@ const {
 
 // Define our grammar
 const grammar = `
-document  <-  __ (object / array) __
-object    <-  '{' pair (',' pair)* '}' / '{' __ '}'
-pair      <-  __ string __ ':' value
-array     <-  '[' value (',' value)* ']' / '[' __ ']'
-value     <-  __ (object / array / string / number / boolean_ / null_) __
-string    <-  '"' ('\\' m/./ / m/[^"]/)* '"'
-number    <-  '-'? ('0' / m/[1-9][0-9]*/) ('.' m/[0-9]+/)? (('e' / 'E') ('+' / '-' / '') m/[0-9]+/)?
-boolean_  <-  'true' / 'false'
-null_     <-  'null'
-__        <-  m/\\s*/
+additive        <- multitive additiveSuffix?
+additiveSuffix  <- '+' additive
+multitive       <- number multitiveSuffix?
+multitiveSuffix <- ('*' / 'x') multitive
+number          <- m/\\d+/
 `;
 
 // Define our actions
 const actions = {
-    null_: match => null, // this time we actually want the value `null`
-    boolean_: match => match.value[0] === 'true' ? true : false,
-    number: match => Number.parseFloat(input.slice(match.start, match.start+match.consumed)),
-    // TODO: does not support unicode escape sequences (probably)
-    string: match => match.value[1].value.map(charMatch => {
-        if (charMatch.alternative === 0) {
-            // handles escape codes
-            return {
-                'b': '\b',
-                'f': '\f',
-                'n': '\n',
-                'r': '\r',
-                't': '\t',
-                '"': '"',
-                '\\': '\\',
-            }[charMatch.value[1]];
-        } else {
-            expect(charMatch.value).to.be.an('array').of.length(1);
-            return charMatch.value[0];
-        }
-    }).join(''),
-    value: match => match.value[1].value[0].value,
-    array: match => {
-        if (match.alternative === 1) {
-            return [];
-        } else {
-            const first = match.value[1].value;
-            const rest = match.value[2].value.map(sub => sub.value[1].value);
-            return [first, ...rest];
-        }
+    number: match => Number.parseInt(match.value),
+    multitiveSuffix: match => match.value[1].value,
+    multitive: match => {
+        const num = match.value[0].value;
+        const suffixes = match.value[1].value;
+        return num * (suffixes.length ? suffixes[0].value : 1);
     },
-    pair: match => [match.value[1].value, match.value[4].value],
-    object: match => {
-        if (match.alternative === 1) {
-            return {};
-        } else {
-            const first = match.value[1].value;
-            const rest = match.value[2].value.map(sub => sub.value[1].value);
-            const pairs = [first, ...rest];
-            const object = {};
-            for (const pair of pairs) {
-                object[pair[0]] = pair[1];
-            }
-            return object;
-        }
+    additiveSuffix: match => match.value[1].value,
+    additive: match => {
+        const num = match.value[0].value;
+        const suffixes = match.value[1].value;
+        return num + (suffixes.length ? suffixes[0].value : 0);
     },
-    document: match => match.value[1].value[0].value,
 };
 
 // Attempt to parse an a value from our input
-const input =`{
-    "vals": [null, true, false, 1.2E3, {}, {"a": 1}, [], [1]],
-    "str": "my\\nstring"
-}`;
-const result = parse('document', input, grammar, actions);
+const input =`1*2+3*4+5`;
+const result = parse('additive', input, grammar, actions);
 p(result);
